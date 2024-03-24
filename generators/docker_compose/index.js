d718889b05c14ceb0a1aaf550767d623af1cea4e
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-24 20:18:31 -0300

    feat(docker-compose): agregado soporte para mariadb, postgresql y mysql

--- generators/docker_compose/index.js
+++ generators/docker_compose/index.js
@@ -1,20 +1,30 @@
 import Generator from 'yeoman-generator'
 
 export default class GeneratorDockerCompose extends Generator {
+  constructor(args, opts) {
+    super(args, opts)
+
+    this.argument('databaseName', { type: String, required: false })
+  }
+
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('database/Dockerfile'),
-      this.destinationPath('database/Dockerfile'),
-      { image_database: 'new_project' },
+    const databaseName = this.options.databaseName || 'mysql'
+
+    this.fs.copy(
+      this.templatePath(`databases/${databaseName}/database`),
+      this.destinationPath('database'),
     )
     this.fs.copy(
-      this.templatePath('database/.env'),
+      this.templatePath(`databases/${databaseName}/database/.env`),
       this.destinationPath('database/.env'),
     )
-    this.fs.copyTpl(
-      this.templatePath('docker-compose.yml'),
+    this.fs.copy(
+      this.templatePath(`databases/${databaseName}/docker-compose.yml`),
       this.destinationPath('docker-compose.yml'),
-      { project_name: 'alallala' },
+    )
+    this.fs.copy(
+      this.templatePath('api/wait-for-it.sh'),
+      this.destinationPath('api/wait-for-it.sh'),
     )
   }
 }
