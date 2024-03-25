c687b65007b632688bd82f751a5966cd9ad3916d
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-25 12:52:58 -0300

    feat(eslint): no se aplica jsdoc a models/user.js

--- generators/sequelize/templates/api/src/db/models/user.js
+++ generators/sequelize/templates/api/src/db/models/user.js
@@ -6,8 +6,6 @@ export default (sequelize, DataTypes) => {
      * Helper method for defining associations. This method is not a part of
      * Sequelize lifecycle. The `models/index` file will call this method
      * automatically.
-     *
-     * @param models
      */
     static associate(models) {
       // define association here
