0a8d41907a33fc351b0994ef8e431cca10d2cfcc
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-25 18:28:06 -0300

    feat(openapi): se agrega configuración de openapi

---
+++ generators/openapi/templates/api/src/config/openapi.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default {
+  cwd: path.resolve(dirname, '..', '..'),
+  extension: ['.js', '.cjs', '.mjs', '.ts', '.tsx', '.jsx', '.yaml', '.yml'],
+  include: ['**'],
+  exclude: [
+    'coverage/**',
+    'packages/*/test{,s}/**',
+    '**/*.d.ts',
+    'test{,s}/**',
+    'test{,-*}.{js,cjs,mjs,ts,tsx,jsx,yaml,yml}',
+    '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx,yaml,yml}',
+    '**/__tests__/**',
+    '**/{ava,babel,nyc}.config.{js,cjs,mjs}',
+    '**/jest.config.{js,cjs,mjs,ts}',
+    '**/{karma,rollup,webpack}.config.js',
+    '**/.{eslint,mocha}rc.{js,cjs}',
+    '**/.{travis,yarnrc}.yml',
+    '**/{docker-compose}.yml',
+  ],
+  excludeNodeModules: true,
+  verbose: true,
+  throwLevel: 'off',
+}
