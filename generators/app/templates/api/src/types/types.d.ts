dfd12d706953ee89cc7f0327685c348916314f20
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-25 17:40:48 -0300

    refactor(app): cambia el nombre de la clase OrmModelStorage y se agregan comentarios adicionales

--- generators/app/templates/api/src/types/types.d.ts
+++ generators/app/templates/api/src/types/types.d.ts
@@ -2,19 +2,21 @@ import { BaseContext } from 'koa'
 import { Sequelize, Model, ModelStatic } from 'sequelize'
 
 class DatabaseConnection {
+  // Connection to database. Instance of Sequelize class.
   sequelize: Sequelize
+  // Sequelize class
   Sequelize: Sequelize
 }
 
 // When you add more models into db/models, add a new property into this class
-class OrmModelStorage {
+class ModelStorage {
   // This property corresponds to User instance for Sequelize Model ubicated in the file db/models/user.js
-  user: ModelStatic<Model>
+  User: ModelStatic<Model>
 }
 
 export class Orm {
   db: DatabaseConnection
-  models: OrmModelStorage
+  models: ModelStorage
 }
 
 // Add property orm into koa context object. Now you can use intelliSense with ctx.orm in koa middleware
