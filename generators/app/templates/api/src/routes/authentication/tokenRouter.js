4fb294d2ce0f6f53ce4525a0897b7ecfc483d89b
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-25 22:21:39 -0300

    feat(app/authentication): se agrega jsdoc a getUserInfo

--- generators/app/templates/api/src/routes/authentication/tokenRouter.js
+++ generators/app/templates/api/src/routes/authentication/tokenRouter.js
@@ -4,6 +4,16 @@ import config from '../../config/jwt.js'
 
 const { JWTSecret, JWTAlgorithm } = config
 
+/** @typedef {import('../../types/types.d.ts').Orm} Orm */
+
+/**
+ * Get user information from database
+ *
+ * @param {string} email User email
+ * @param {string} password User password
+ * @param {Orm} orm Orm object
+ * @returns {Object} User information
+ */
 const getUserInfo = async (email, password, orm) => {
   const user = await orm.models.User.findOne({
     attributes: ['email', 'names', 'surnames'],
