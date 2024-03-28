bd148188661e8ab794c4a2bdd27ed500df0fa86e
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-28 17:03:14 -0300

    feat(generators/docker-compose): se agrega el prompt para soporte de docker-compose

--- generators/docker_compose/index.js
+++ generators/docker_compose/index.js
@@ -1,26 +1,83 @@
 import Generator from 'yeoman-generator'
 
 export default class GeneratorDockerCompose extends Generator {
+  #answers
+
   constructor(args, opts) {
     super(args, opts)
 
+    this.argument('useDockerCompose', { type: Boolean, required: false })
     this.argument('databaseName', { type: String, required: false })
   }
 
-  writing() {
-    const databaseName = this.options.databaseName || 'mysql'
+  async prompting() {
+    const prompts = [
+      {
+        type: 'list',
+        name: 'useDockerCompose',
+        message: 'Add Docker Compose support.',
+        when: () => !this.options.useDockerCompose,
+        choices: [
+          {
+            name: 'yes',
+            value: true,
+          },
+          {
+            name: 'no',
+            value: false,
+          },
+        ],
+      },
+      {
+        type: 'list',
+        name: 'databaseName',
+        message: 'Select a database to connect',
+        when: (answers) =>
+          (this.options.useDockerCompose || answers.useDockerCompose) &&
+          !this.options.databaseName,
+        choices: [
+          {
+            name: 'MySQL',
+            value: 'mysql',
+          },
+          {
+            name: 'PostgreSQL',
+            value: 'postgresql',
+          },
+          {
+            name: 'MariaDB',
+            value: 'mariadb',
+          },
+        ],
+      },
+    ]
 
-    this.fs.copy(
-      this.templatePath(`databases/${databaseName}/database`),
-      this.destinationPath('database'),
-    )
-    this.fs.copy(
-      this.templatePath(`databases/${databaseName}/database/.env`),
-      this.destinationPath('database/.env'),
-    )
-    this.fs.copy(
-      this.templatePath(`databases/${databaseName}/docker-compose.yml`),
-      this.destinationPath('docker-compose.yml'),
-    )
+    const answers = await this.prompt(prompts)
+
+    this.#answers = {
+      useDockerCompose:
+        this.options.useDockerCompose || answers.useDockerCompose || false,
+      databaseName:
+        this.options.databaseName || answers.databaseName || 'postgresql',
+    }
+  }
+
+  writing() {
+    const { useDockerCompose, databaseName } = this.#answers
+
+    if (useDockerCompose) {
+      this.fs.copy(
+        this.templatePath(`databases/${databaseName}/database`),
+        this.destinationPath('database'),
+      )
+      this.fs.copy(
+        this.templatePath(`databases/${databaseName}/database/.env`),
+        this.destinationPath('database/.env'),
+      )
+      this.fs.copy(
+        this.templatePath(`databases/${databaseName}/docker-compose.yml`),
+        this.destinationPath('docker-compose.yml'),
+      )
+    }
   }
 }
