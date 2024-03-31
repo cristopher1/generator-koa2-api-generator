6e26a9b947c46fdafb10589489774e52a74c0647
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-03-31 13:32:48 -0300

    feat(docker-entrypoint.sh): las migraciones se ejecutan en entorno de desarrollo

--- generators/docker/templates/api/docker-entrypoint.sh
+++ generators/docker/templates/api/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=45 -- npx sequelize-cli db:migrate
+# Run migrations when NODE_ENV is development
+if [[ ${NODE_ENV} = "development" ]]
+then
+  ./wait-for-it.sh ${DB_HOST}:${DB_PORT} --timeout=45 -- npx sequelize-cli db:migrate
+fi
 
 exec "$@"
